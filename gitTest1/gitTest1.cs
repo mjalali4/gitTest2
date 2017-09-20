69e986d9e9b6c741e054e73d99144fd74ef721a2
mj <morteza4048@gmail.com>
2017-09-20 22:42:09 +0530

    commit 1.0

---
+++ gitTest1/gitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace gitTest1
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "gitTest1",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+
+
+
+            System.Diagnostics.Debug.WriteLine("xyz");
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
